e821b75b389b19b04c7bf706d97194c051617461
Vivek Mane <vivekmane172@gmail.com>
2025-08-12 12:50:45 +0000

    File based routing

---
+++ Day 01/my-app/app/about/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='text-xl'>Problems solved by Next.js</h1>
+      <ul>
+        <li>Full Stack Solutions</li>
+        <li>File based rounting</li>
+        <li>Additional features like router from next/navigation</li>
+        <li>Optimized Rendering</li>
+      </ul>
+    </div>
+  )
+}
+
+export default page
